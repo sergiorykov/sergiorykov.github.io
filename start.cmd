55f3c54f0e91398af29f1c0c4b5e7c35d4c8e2be
Sergio Rykov <sergior@bk.ru>
2016-11-13 16:20:19 +0300

    * start.cmd enabled --watch on jekyll serve

--- start.cmd
+++ start.cmd
@@ -2,7 +2,8 @@ where jekyll
 
 pushd %~dp0
 	rem run as admin
-	start cmd /k "jekyll s"
+	start cmd /k "jekyll s --port 4000 --host 0.0.0.0 --watch"
 	sleep 10
-	start http://localhost:4000/
+	rem start http://localhost:4000/
+	start http://77.37.146.206:4000/
 popd
