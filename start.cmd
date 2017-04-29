20ad3ae1fea73e8931ef504f6fd7e1173c4474b2
Sergio Rykov <sergior@bk.ru>
2017-04-29 18:39:14 +0300

    start.cmd opens localhost again

--- start.cmd
+++ start.cmd
@@ -4,6 +4,6 @@ pushd %~dp0
 	rem run as admin
 	start cmd /k "jekyll s --port 4000 --host 0.0.0.0 --watch"
 	sleep 10
-	rem start http://localhost:4000/
-	start http://77.37.146.206:4000/
+	start http://localhost:4000/
+	rem start http://77.37.146.206:4000/
 popd
